dc5da82074a7f5c5af1bb3b437e9c4ce3e33972e
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2023-07-07 15:58:03 +0200

    Add Instrument abstraction

--- src/dto.rs
+++ src/dto.rs
@@ -7,6 +7,9 @@ pub struct Attendee {
     pub tastes: Vec<f32>,
 }
 
+#[derive(Serialize, Deserialize, Debug, Default, Clone, Copy)]
+pub struct Instrument(pub u32);
+
 // Default is to avoid Option<ProblemDto> in solvers
 #[derive(Serialize, Deserialize, Debug, Default, Clone)]
 pub struct ProblemDto {
@@ -15,7 +18,7 @@ pub struct ProblemDto {
     pub stage_width: f32,
     pub stage_height: f32,
     pub stage_bottom_left: (f32, f32),
-    pub musicians: Vec<i32>,
+    pub musicians: Vec<Instrument>,
     pub attendees: Vec<Attendee>,
 }
 
